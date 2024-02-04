19fab6e4bab9166486d6eea89edc75c02eac0601
Tristan Gerritsen <code@thewoosh.org>
2024-02-04 10:46:42 +0100

    fix: Attach label text hook to setStringValue instead of setTitle
    This caused a nice libc++ and Objective-C exception :^(

--- finestra/src/platform/macos/state.rs
+++ finestra/src/platform/macos/state.rs
@@ -11,6 +11,7 @@ use crate::{event::ViewId, Button, Label};
 
 use super::resources::ToCacao;
 
+#[derive(Debug)]
 pub enum Event {
     ButtonClicked(ViewId),
 }
@@ -30,7 +31,7 @@ pub fn attach_label_state<S>(finestra: &Label<S>, cacao: &CacaoLabel) {
     attach_base_state(finestra, &cacao.objc);
     hook_background_color_state(&cacao.objc, &finestra.background_color);
     hook_text_color_state(&cacao.objc, &finestra.text_color);
-    hook_title_state(&cacao.objc, &finestra.text);
+    hook_string_value_state(&cacao.objc, &finestra.text);
 }
 
 fn hook_background_color_state(objc: &ObjcProperty, color: &StateOrRaw<Color>) {
@@ -46,6 +47,20 @@ fn hook_background_color_state(objc: &ObjcProperty, color: &StateOrRaw<Color>) {
             let color = color.as_ref().cg_color();
             let layer: id = msg_send![obj, layer];
             let _: () = msg_send![layer, setBackgroundColor: color];
+        });
+    });
+}
+fn hook_string_value_state(objc: &ObjcProperty, text: &StateOrRaw<String>) {
+    let StateOrRaw::State(text_state) = &text else {
+        return;
+    };
+
+    let objc = objc.clone();
+    text_state.add_listener(move |val| {
+        let s = NSString::new(val);
+
+        objc.with_mut(|obj| unsafe {
+            let _: () = msg_send![obj, setStringValue:&*s];
         });
     });
 }
