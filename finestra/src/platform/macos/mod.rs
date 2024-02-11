5af114e4161538d4dad4f77e5008ade921a9e155
Tristan Gerritsen <code@thewoosh.org>
2024-02-11 19:44:16 +0100

    chore: Refactor macOS-specific Window-stuff

--- finestra/src/platform/macos/mod.rs
+++ finestra/src/platform/macos/mod.rs
@@ -6,30 +6,29 @@ mod dynamic_wrapper;
 pub(crate) mod extensions;
 pub(crate) mod objc;
 pub(crate) mod resources;
+mod menu;
 pub(crate) mod state;
 pub(crate) mod views;
-mod menu;
+pub(crate) mod window;
 
-use std::borrow::Cow;
+pub(crate) use self::dynamic_wrapper::DynamicViewWrapper;
+pub(crate) use self::dynamic_wrapper::LayoutExt;
+pub(crate) use self::objc::*;
+pub(crate) use self::views::*;
+
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 
 use cacao::appkit::{App as CacaoApp, AppDelegate as CacaoAppDelegate};
-use cacao::appkit::window::{Window as CacaoWindow, WindowDelegate};
-use cacao::layout::LayoutConstraint;
+use cacao::appkit::window::Window as CacaoWindow;
 use cacao::notification_center::Dispatcher;
 
-use crate::event::{EventHandlerMapRegistry, ViewTree};
-use crate::platform::macos::cacao_delegates::StatefulEventDispatcher;
-use crate::{App, AppDelegate, DialogBuilder, StateChangeOrigin, View, Window, WindowDelegator};
-pub(crate) use self::dynamic_wrapper::DynamicViewWrapper;
-pub(crate) use self::dynamic_wrapper::LayoutExt;
-use self::extensions::WindowExtensions;
-pub(crate) use self::objc::*;
-use self::state::Event;
-pub(crate) use self::views::*;
+use crate::{App, AppDelegate, StateChangeOrigin, EventHandlerMapRegistry, Window};
 
+use self::extensions::WindowExtensions;
+use self::state::Event;
+use self::window::{MacOSWindowDelegate, MacOSWindowDelegator};
 
 const BUNDLE_ID: &str = "com.tristangerritsen.finestra";
 
@@ -177,76 +176,5 @@ impl<Delegate, State> Dispatcher for MacOSDelegate<Delegate, State>
                 (handler)(&mut state, text, window);
             }
         }
-    }
-}
-
-struct MacOSWindowDelegate<Delegate, State>
-        where State: 'static {
-    delegate: Rc<RefCell<Delegate>>,
-    window: Option<Rc<CacaoWindow>>,
-    #[allow(unused)]
-    delegator: Option<Window>,
-    view: cacao::view::View,
-    content: Option<DynamicViewWrapper>,
-    event_registry: EventHandlerMapRegistry<State>,
-    state: Arc<Mutex<State>>,
-}
-
-impl<Delegate, State> WindowDelegate for MacOSWindowDelegate<Delegate, State>
-        where Delegate: AppDelegate<State> {
-    const NAME: &'static str = "finestra";
-
-    fn did_load(&mut self, window: CacaoWindow) {
-        let window = Rc::new(window);
-        debug_assert!(self.window.is_none());
-
-        let user_delegator = Window::new(Arc::new(MacOSWindowDelegator {
-            window: Rc::clone(&window),
-        }));
-
-        let dispatcher = StatefulEventDispatcher {
-            state: self.state.clone(),
-            event_registry: self.event_registry.clone(),
-            window: user_delegator.clone(),
-        };
-
-        let mut delegate = self.delegate.as_ref().borrow_mut();
-
-        let mut state = self.state.lock().unwrap();
-        let mut content_view = delegate.make_content_view(&mut state, user_delegator);
-
-        let mut tree = ViewTree::new(self.event_registry.clone(), dispatcher);
-        let parent_id = tree.exchange_events_for_id(Default::default());
-        tree.set_parent_id(parent_id);
-        let content_view = content_view.build_native(&mut tree);
-
-        content_view.add_to_view(&self.view);
-
-        window.set_content_view(&self.view);
-
-        if content_view.constraints().is_empty() {
-            LayoutConstraint::activate(&[
-                content_view.layout_constraint_center_x().constraint_equal_to(&self.view.center_x),
-                content_view.layout_constraint_center_y().constraint_equal_to(&self.view.center_y),
-            ]);
-        } else {
-            for constraint in content_view.constraints() {
-                let constraint = constraint.convert(&content_view, &self.view);
-                constraint.set_active(true);
-            }
-        }
-
-        self.content = Some(content_view);
-        self.window = Some(window);
-    }
-}
-
-struct MacOSWindowDelegator<CacaoDelegate> {
-    window: Rc<CacaoWindow<CacaoDelegate>>,
-}
-
-impl<CacaoDelegate> WindowDelegator for MacOSWindowDelegator<CacaoDelegate> {
-    fn create_dialog(&self, text: Cow<'static, str>) -> crate::DialogBuilder {
-        DialogBuilder::new(Box::new(NSAlert::with(text, self.window.get_title())))
     }
 }
---
+++ finestra/src/platform/macos/window.rs
@@ -0,0 +1,90 @@
+// Copyright (C) 2024 Tristan Gerritsen <tristan@thewoosh.org>
+// All Rights Reserved.
+
+use std::borrow::Cow;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+
+use cacao::appkit::window::{Window as CacaoWindow, WindowDelegate};
+use cacao::layout::LayoutConstraint;
+
+use crate::event::{EventHandlerMapRegistry, ViewTree};
+use super::cacao_delegates::StatefulEventDispatcher;
+use crate::{AppDelegate, DialogBuilder, View, Window, WindowDelegator};
+use super::dynamic_wrapper::DynamicViewWrapper;
+use super::dynamic_wrapper::LayoutExt;
+use super::extensions::WindowExtensions;
+use super::views::NSAlert;
+
+pub(super) struct MacOSWindowDelegate<Delegate, State>
+        where State: 'static {
+    pub(super) delegate: Rc<RefCell<Delegate>>,
+    pub(super) window: Option<Rc<CacaoWindow>>,
+
+    #[allow(unused)]
+    pub(super) delegator: Option<Window>,
+    pub(super) view: cacao::view::View,
+    pub(super) content: Option<DynamicViewWrapper>,
+    pub(super) event_registry: EventHandlerMapRegistry<State>,
+    pub(super) state: Arc<Mutex<State>>,
+}
+
+impl<Delegate, State> WindowDelegate for MacOSWindowDelegate<Delegate, State>
+        where Delegate: AppDelegate<State> {
+    const NAME: &'static str = "finestra";
+
+    fn did_load(&mut self, window: CacaoWindow) {
+        let window = Rc::new(window);
+        debug_assert!(self.window.is_none());
+
+        let user_delegator = Window::new(Arc::new(MacOSWindowDelegator {
+            window: Rc::clone(&window),
+        }));
+
+        let dispatcher = StatefulEventDispatcher {
+            state: self.state.clone(),
+            event_registry: self.event_registry.clone(),
+            window: user_delegator.clone(),
+        };
+
+        let mut delegate = self.delegate.as_ref().borrow_mut();
+
+        let mut state = self.state.lock().unwrap();
+        let mut content_view = delegate.make_content_view(&mut state, user_delegator);
+
+        let mut tree = ViewTree::new(self.event_registry.clone(), dispatcher);
+        let parent_id = tree.exchange_events_for_id(Default::default());
+        tree.set_parent_id(parent_id);
+        let content_view = content_view.build_native(&mut tree);
+
+        content_view.add_to_view(&self.view);
+
+        window.set_content_view(&self.view);
+
+        if content_view.constraints().is_empty() {
+            LayoutConstraint::activate(&[
+                content_view.layout_constraint_center_x().constraint_equal_to(&self.view.center_x),
+                content_view.layout_constraint_center_y().constraint_equal_to(&self.view.center_y),
+            ]);
+        } else {
+            for constraint in content_view.constraints() {
+                let constraint = constraint.convert(&content_view, &self.view);
+                constraint.set_active(true);
+            }
+        }
+
+        self.content = Some(content_view);
+        self.window = Some(window);
+    }
+}
+
+pub(super) struct MacOSWindowDelegator<CacaoDelegate> {
+    pub(super) window: Rc<CacaoWindow<CacaoDelegate>>,
+}
+
+impl<CacaoDelegate> WindowDelegator for MacOSWindowDelegator<CacaoDelegate> {
+    fn create_dialog(&self, text: Cow<'static, str>) -> crate::DialogBuilder {
+        DialogBuilder::new(Box::new(NSAlert::with(text, self.window.get_title())))
+    }
+}
